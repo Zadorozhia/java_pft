995898e703d817a4369e0fc66257c57f43ba0e76
Viktoria_Z <viktory3005@gmail.com>
2016-07-20 15:51:44 +0300

    Добавлен конструктор и функция преобразована в метод.
    Пока не работает.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,6 +4,17 @@ package ru.stqa.pft.sandbox;
  * Created by Виктория on 19.07.2016.
  */
 public class Point {
-  int x;
-  int y;
+  public double x;
+  public double y;
+
+  public Point(double x,double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double distance(){
+    double dx=this.x-x;
+    double dy=this.y-y;
+    return Math.sqrt(dx*dx+dy*dy) ;
+  }
+
 }
