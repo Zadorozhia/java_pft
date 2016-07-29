f791e7eee5998d9249e8d6afaa29bc89391a52ac
Viktoria_Z <viktory3005@gmail.com>
2016-07-29 21:47:30 +0300

    Применила наследование для Contacts
    ОШИБКИ!!!

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.*;
+
+public class ContactCreationTests extends TestBase{
+
+
+
+  @Test
+  public void testContactCreation() {
+
+    initContactCreation();
+    fillContactForm(new ContactDataName("user1", "user2", "user3", "user4"), new ContactDataInfo("user5", "user6", "https://user.ru", "2000", "2010", "user10"), new ContactDataAddress("user7", "user8"), new ContactDataPhone("000000", "111111", "222222", "333333", "user9"), new ContactDataMail("mail1@mail.ru", "mail2@mail.ru", "mail3@mail.ru"));
+    submitContactCreation();
+  }
+
+
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,24 +1,138 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Виктория on 27.07.2016.
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager();FirefoxDriver wd;
+
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+      try {
+          wd.switchTo().alert();
+          return true;
+      } catch (NoAlertPresentException e) {
+          return false;
+      }
+  }
 
   @BeforeMethod
   public void setUp() throws Exception {
     app.init();
   }
 
+  protected void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactDataName contactDataName, ContactDataInfo contactDataInfo, ContactDataAddress contactDataAddress, ContactDataPhone contactDataPhone, ContactDataMail contactDataMail) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactDataName.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(contactDataName.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactDataName.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactDataName.getNickname());
+    wd.findElement(By.name("photo")).click();
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactDataInfo.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactDataInfo.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactDataAddress.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactDataPhone.getHome());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactDataPhone.getMobile());
+    wd.findElement(By.name("work")).click();
+    wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys(contactDataPhone.getWork());
+    wd.findElement(By.name("fax")).click();
+    wd.findElement(By.name("fax")).clear();
+    wd.findElement(By.name("fax")).sendKeys(contactDataPhone.getFax());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactDataMail.getEmail());
+    wd.findElement(By.name("email2")).click();
+    wd.findElement(By.name("email2")).clear();
+    wd.findElement(By.name("email2")).sendKeys(contactDataMail.getEmail2());
+    wd.findElement(By.name("email3")).click();
+    wd.findElement(By.name("email3")).clear();
+    wd.findElement(By.name("email3")).sendKeys(contactDataMail.getEmail3());
+    wd.findElement(By.name("homepage")).click();
+    wd.findElement(By.name("homepage")).clear();
+    wd.findElement(By.name("homepage")).sendKeys(contactDataInfo.getHomepage());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).click();
+    }
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).click();
+    }
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(contactDataInfo.getByear());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[3]//option[3]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[3]//option[3]")).click();
+    }
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[4]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[4]//option[2]")).click();
+    }
+    wd.findElement(By.name("ayear")).click();
+    wd.findElement(By.name("ayear")).clear();
+    wd.findElement(By.name("ayear")).sendKeys(contactDataInfo.getAyear());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
+    }
+    wd.findElement(By.name("address2")).click();
+    wd.findElement(By.name("address2")).clear();
+    wd.findElement(By.name("address2")).sendKeys(contactDataAddress.getAddress2());
+    wd.findElement(By.name("phone2")).click();
+    wd.findElement(By.name("phone2")).clear();
+    wd.findElement(By.name("phone2")).sendKeys(contactDataPhone.getPhone2());
+    wd.findElement(By.name("notes")).click();
+    wd.findElement(By.name("notes")).clear();
+    wd.findElement(By.name("notes")).sendKeys(contactDataInfo.getNotes());
+  }
+
+  protected void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+
+
   @AfterMethod
   public void tearDown() {
     app.stop();
   }
 
+
+
+  protected void deleteSelectedGroup() {
+      wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+  }
+
+  protected void selectContact() {
+      wd.findElement(By.name("selected[]")).click();
+  }
 }
