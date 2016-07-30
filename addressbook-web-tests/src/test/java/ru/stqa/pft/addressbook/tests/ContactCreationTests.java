a579a0dfc8519033f931396a3aa28b373f9c658f
Viktoria_Z <viktory3005@gmail.com>
2016-07-30 11:11:49 +0300

    Делегирование для тестирования контактов.
    Запуск успешный!

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import ru.stqa.pft.addressbook.*;
-import ru.stqa.pft.addressbook.tests.TestBase;
+import ru.stqa.pft.addressbook.model.*;
 
 public class ContactCreationTests extends TestBase{
 
@@ -18,10 +11,12 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation() {
 
-    initContactCreation();
-    fillContactForm(new ContactDataName("user1", "user2", "user3", "user4"), new ContactDataInfo("user5", "user6", "https://user.ru", "2000", "2010", "user10"), new ContactDataAddress("user7", "user8"), new ContactDataPhone("000000", "111111", "222222", "333333", "user9"), new ContactDataMail("mail1@mail.ru", "mail2@mail.ru", "mail3@mail.ru"));
-    submitContactCreation();
+    app.initContactCreation();
+    app.fillContactForm(new ContactDataName("user1", "user2", "user3", "user4"), new ContactDataInfo("user5", "user6", "https://user.ru", "2000", "2010", "user10"), new ContactDataAddress("user7", "user8"), new ContactDataPhone("000000", "111111", "222222", "333333", "user9"), new ContactDataMail("mail1@mail.ru", "mail2@mail.ru", "mail3@mail.ru"));
+    app.submitContactCreation();
   }
+
+
 
 
 }
