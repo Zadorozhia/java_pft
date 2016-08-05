d8aaf27e45cc0b849e4dc8814745bd08258f4b31
Viktoria_Z <viktory3005@gmail.com>
2016-08-05 19:09:01 +0300

    Реализована проверка и обеспечение выполнения предусловий для групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -48,4 +48,14 @@ public class GroupHelper extends HelperBase{
   }
 
 
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
