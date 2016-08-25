ff541e0d651125539e1a83e28d65205c330c12b7
Viktoria_Z <viktory3005@gmail.com>
2016-08-25 20:02:42 +0300

    Ошибка после обьединения.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.*;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.Comparator;
 import java.util.List;
@@ -11,25 +11,22 @@ public class ContactCreationTests extends TestBase{
 
 
 
-  @Test(enabled=false)
+  @Test
   public void testContactCreation() {
 
 
-    List<ContactDataName> before=app.getContactHelper().getContactList();
-    ContactDataName contact=new ContactDataName("userLam", "user2", "user3", "user4");
-    app.getContactHelper().createContact(contact,
-            new ContactDataInfo(null, null, "https://user.ru", "2000", "2010", null, "test1"),
-            new ContactDataAddress("user7", "user8"),
-            new ContactDataPhone("000000", "111111", "222222", "333333", "user9"),
-            new ContactDataMail("mail1@mail.ru", "mail2@mail.ru", "mail3@mail.ru"));
+    List<ContactData> before=app.getContactHelper().getContactList();
+    ContactData contact=new ContactData("userLam", "user2", "user3", "user4",null, null, "https://user.ru", "2000", "2010", null, "test1",
+            "user7", "user8","000000", "111111", "222222", "333333", "user9","mail1@mail.ru", "mail2@mail.ru", "mail3@mail.ru");
+    app.getContactHelper().createContact(contact);
     app.goTo().gotoHomePage();
 
-    List<ContactDataName> after=app.getContactHelper().getContactList();
+    List<ContactData> after=app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size()+1);
 
 
     before.add(contact);
-    Comparator<? super ContactDataName> byId=(g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    Comparator<? super ContactData> byId=(g1, g2) -> Integer.compare(g1.getId(),g2.getId());
     before.sort(byId);
     after.sort(byId);
     Assert.assertEquals(before,after);
