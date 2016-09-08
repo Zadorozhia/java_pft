008aab3f005884663bb6cc32edc28e0e68681e4f
Viktoria_Z <viktory3005@gmail.com>
2016-09-08 20:06:43 +0300

    Добавлен провайдер для формата json.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -23,7 +25,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
 
     BufferedReader reader=new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml="";
@@ -37,8 +39,23 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> groups=(List<GroupData>) xstream.fromXML(xml);
     return groups.stream().map((g)->new Object[]{g}).collect(Collectors.toList()).iterator();
   }
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
 
-  @Test(dataProvider = "validGroups")
+    BufferedReader reader=new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json="";
+    String line=reader.readLine();
+    while (line!=null){
+      json+=line;
+      line=reader.readLine();
+    }
+
+    Gson gson=new Gson();
+    List<GroupData> groups=gson.fromJson(json,new TypeToken<List<GroupData>>()
+    {}.getType());
+    return groups.stream().map((g)->new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
 
     app.goTo().groupPage();
